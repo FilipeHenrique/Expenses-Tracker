f76196fc87c47a17fe181ad0d7db0d6f84394d0e
Filipe <filipevp1999@hotmail.com>
2023-08-02 19:43:06 -0300

    feat: piechart label and legend upupdated

--- web/src/app/(pages)/dashboard/components/expenses-piechart.tsx
+++ web/src/app/(pages)/dashboard/components/expenses-piechart.tsx
@@ -1,53 +1,77 @@
 "use client";
-import { PieChart, Pie, Legend, Cell } from "recharts";
+import { PieChart, Pie, Legend, Cell, ResponsiveContainer } from "recharts";
 import colors from "../colors";
 
 export default function ExpensesPieChart() {
-  const data01 = [
+  const data = [
     {
-      name: "Group A",
+      name: "Miscellaneous",
       value: 400,
     },
     {
-      name: "Group B",
+      name: "Food",
       value: 300,
     },
     {
-      name: "Group C",
+      name: "Utilities",
       value: 300,
     },
     {
-      name: "Group D",
+      name: "Shopping",
       value: 200,
     },
     {
-      name: "Group E",
+      name: "Transportation",
       value: 278,
     },
     {
-      name: "Group F",
+      name: "Insurance",
       value: 189,
     },
   ];
 
+  const renderLabel = function (labelData: any) {
+    labelData.fill = "black";
+    return labelData.name;
+  };
+
+  const total = data.reduce((sum, entry) => sum + entry.value, 0);
+
   return (
+    // <ResponsiveContainer width="80%" height="80%">
     <PieChart width={730} height={300}>
       <Pie
+        isAnimationActive={true}
         className="outline-none"
-        data={data01}
+        data={data}
         dataKey="value"
         nameKey="name"
         cx="50%"
         cy="50%"
         innerRadius={60}
         outerRadius={130}
-        label
+        label={renderLabel}
       >
-        {data01.map((entry, index) => (
+        {data.map((_, index) => (
           <Cell key={index} fill={colors[index % colors.length]} />
         ))}
       </Pie>
-      <Legend layout="vertical" align="right" verticalAlign="top" height={36} />
+      <Legend
+        layout="vertical"
+        align="right"
+        verticalAlign="top"
+        height={36}
+        formatter={(value, entry) => (
+          <span className="text-sm font-normal text-black">
+            {`${value}`}
+            {"  "}
+            {`R$${entry.payload?.value}`}
+            {"  "}
+            {`(${((entry.payload?.value / total) * 100).toFixed(0)}%) `}
+          </span>
+        )}
+      />
     </PieChart>
+    // </ResponsiveContainer>
   );
 }
